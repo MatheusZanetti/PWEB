f8695ff67118df4776e052099e558fd62e5a26a8
MatheusZanetti <149878042+MatheusZanetti@users.noreply.github.com>
2025-05-12 12:39:57 -0300

    Add files via upload

---
+++ Atividade15/Atividade15.js
@@ -0,0 +1,10 @@
+
+function validarDate(){
+    if(document.getElementById("sim").checked){
+    alert("Volte Sempre para essa página!");
+    
+    }
+    else{
+    alert("Que bom que você já visitou essa página");    
+    }
+}
